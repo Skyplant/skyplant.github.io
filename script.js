b5355dc8892527bd43b83a4cd31cc3c3c93c7251
Skyplant <127964484+Skyplant@users.noreply.github.com>
2023-03-15 15:18:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-google crome">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>The rockin roll | help by Coral</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <header>
+        <h2 class="logo">Velkommen til The rockin roll</h2>
+        <nav class="navigation">
+            <a href="#">Forside</a>
+            <a href="#">Shop</a>
+            <a href="#">Om</a>
+            <a href="#">Booking</a>
+            <a href="#">Kontakt os</a>
+            <button class="btnLogin-popup">Login</button>
+        </nav>
+    </header>
+
+
+    <script src="script.js"></script>
+</body>
+
+</html>
